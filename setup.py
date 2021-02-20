8aff70ae97ea19759a128cf1ac2afe4245dae690
Conrad Storz <conradstorz@gmail.com>
2021-02-20 17:27:11 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="CFSIV_utilities_package", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Conrad Storz IV",
     author_email="conradstorz@gmail.com",
     description="A collection of Time, Date, Filehandling and webscraping functions",
