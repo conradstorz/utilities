5f8ae8028c12a6202f31fcfa940ca7d1da4f90f3
Conrad Storz <conradstorz@gmail.com>
2022-08-09 08:38:18 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="CFSIV-utils-Conradical",
-    version="0.1.4",
+    version="0.1.5",
     author="Conrad Storz IV",
     author_email="conradstorz@gmail.com",
     description="A collection of Logging, Time, Date, Filehandling and webscraping functions",
